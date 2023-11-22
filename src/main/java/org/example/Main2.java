59d1893589647927507bed75b8db8a5df1af642d
jubayerh <jubayer@hotmail.com>
2023-11-22 11:56:15 -0500

    8-new class is add

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
   void test(){
       System.out.println("hello this test");
       System.out.println("good moring ");
+      System.out.println("hello how are you");
 
     }
 }
